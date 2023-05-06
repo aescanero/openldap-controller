5a7d8aa8db81a0112bffcbebbcd8cb2fd510cc2c
Alejandro Escanero Blanco <aescanero@gmail.com>
2023-05-06 21:51:24 +0200

    Add micropki 0.1.2 to create sample certificates

--- sample/standalone.sh
+++ sample/standalone.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-podman run -v ./standalone:/config --name node --entrypoint '["./controller","start","--config_file=/config/config.yaml"]' --rm -it localhost/openldap-node:0.1.1
+podman run -v ./standalone:/config --name micropki --entrypoint '["./micropki","cert","new","--cafile","/config/ca.crt","--cakeyfile","/config/ca.key","--certfile","/config/cert.crt","--certkeyfile","/config/cert.key"]' --rm -it docker.io/aescanero/micropki:0.1.2-linux-amd64
+
+podman run -v ./standalone:/config --name node --entrypoint '["./controller","start","--config_file=/config/config.yaml"]' --rm -it docker.io/aescanero/openldap-node:0.1.2-linux-amd64
