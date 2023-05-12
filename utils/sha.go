5b71cb485039154573545399cdc4ebf956cb2e05
Alejandro Escanero Blanco <aescanero@gmail.com>
2023-05-12 18:38:35 +0200

    Added match for tests

--- utils/sha.go
+++ utils/sha.go
@@ -15,9 +15,12 @@ limitations under the License.*/
 package utils
 
 import (
+	"bytes"
+	"crypto/rand"
 	"crypto/sha1"
 	"crypto/sha256"
-	"math/rand"
+	"encoding/base64"
+	"log"
 )
 
 type Encode struct{}
@@ -40,4 +43,38 @@ func (enc Encode) MakeSSHA256Encode(pass []byte) (passEncode []byte) {
 	sha.Write(salt)
 	h := sha.Sum(nil)
 	return append(h, salt...)
+}
+
+func (enc Encode) Matches(passwordSSHAB64, rawPassPhrase []byte, debug bool) bool {
+	//strip the {SSHA}
+	if debug {
+		log.Printf("pass: %s\n", string(rawPassPhrase))
+		log.Printf("passSSHAB64: %s\n", string(passwordSSHAB64))
+	}
+	eppS := string(passwordSSHAB64)[6:]
+	if debug {
+		log.Printf("eppS: %s\n", eppS)
+	}
+	hash, err := base64.StdEncoding.DecodeString(eppS)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if debug {
+		log.Printf("hash: %s\n", string(hash))
+	}
+	salt := hash[len(hash)-4:]
+	if debug {
+		log.Printf("salt: %s\n", string(salt))
+	}
+	sha := sha1.New()
+	sha.Write(rawPassPhrase)
+	sha.Write(salt)
+	sum := sha.Sum(nil)
+	if debug {
+		log.Printf("sum: %s\n", string(sum))
+	}
+	if debug {
+		log.Printf("hash: %s\n", string(hash[:len(hash)-4]))
+	}
+	return bytes.Equal(sum, hash[:len(hash)-4])
 }
