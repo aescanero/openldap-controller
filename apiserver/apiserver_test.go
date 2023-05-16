450aff5d7d3d2c656ae48df4b87537e99b977fa6
Alejandro Escanero Blanco <aescanero@gmail.com>
2023-05-16 23:55:30 +0200

    Test for json response

--- apiserver/apiserver_test.go
+++ apiserver/apiserver_test.go
@@ -1,6 +1,7 @@
 package apiserver
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"log"
@@ -40,5 +41,36 @@ func TestBase(t *testing.T) {
 
 	if got != expected {
 		t.Errorf("got %s, expected %s", got, expected)
+	}
+}
+
+func TestJSON(t *testing.T) {
+
+	type message struct {
+		Id    string `json:"id"`
+		Value string `json:"value"`
+	}
+
+	type rlist struct {
+		R []message
+	}
+
+	var Response rlist
+	Response.R = append(Response.R, message{"monitorOpCompleted", "0"})
+	Response.R = append(Response.R, message{"monitorOpFailed", "0"})
+	Response.R = append(Response.R, message{"monitorOpAborted", "0"})
+	Response.R = append(Response.R, message{"monitorOpCancelled", "0"})
+	Response.R = append(Response.R, message{"monitorOpTimeout", "0"})
+
+	for _, v := range Response.R {
+		vjson, _ := json.Marshal(v)
+		fmt.Println(v.Id, v.Value, string(vjson))
+	}
+
+	expected := `[{"id":"monitorOpCompleted","value":"0"},{"id":"monitorOpFailed","value":"0"},{"id":"monitorOpAborted","value":"0"},{"id":"monitorOpCancelled","value":"0"},{"id":"monitorOpTimeout","value":"0"}]`
+	got, _ := json.Marshal(Response.R)
+
+	if string(got) != expected {
+		t.Errorf("got %s, expected %s", string(got), expected)
 	}
 }
