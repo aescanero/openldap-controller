315ffaeb3370dde69121be634587f2400455dd77
Alejandro Escanero Blanco <aescanero@gmail.com>
2023-05-21 13:18:36 +0200

    Corrected with debug

--- apiserver/monitor.go
+++ apiserver/monitor.go
@@ -2,10 +2,11 @@ package apiserver
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/aescanero/openldap-node/config"
 	"github.com/aescanero/openldap-node/ldaputils"
@@ -47,25 +48,35 @@ func monitor(ctx *gin.Context, apiconfig config.Config) {
 		log.Fatalf("Failed to search in monitor. %s", err)
 	}
 
-	message, _ := json.Marshal(bind)
-	fmt.Println(string(message))
-
-	ctx.JSON(http.StatusOK, gin.H{"message": message})
-}
-
-func basicAuth(c *gin.Context, apiconfig config.Config) error {
-	user, password, ok := c.Request.BasicAuth()
-	if !ok {
-		c.Abort()
-		c.Writer.WriteHeader(http.StatusUnauthorized)
-		return errors.New("WWW-Authenticate")
-	} else {
-		_, err := ldaputils.Connect(apiconfig, user, password)
-		if err != nil {
-			c.Abort()
-			c.Writer.WriteHeader(http.StatusUnauthorized)
-			return errors.New("WWW-Authenticate")
-		}
+	type message struct {
+		Time   int64    `json:"time"`
+		Legend []string `json:"legend"`
+		Value  []int64  `json:"value"`
 	}
-	return nil
+
+	var Message message
+	var legends []string
+	var values []int64
+
+	now := time.Now().Unix()
+	Message.Time = now
+
+	for x, y := range bind {
+		//Response.R = append(Response.R, message{x, y})
+		legends = append(legends, x)
+		yNum, err := strconv.ParseInt(y, 10, 64)
+		if err != nil {
+			log.Fatalf("Failed to convert to int64: %s. %s", y, err)
+		}
+		values = append(values, yNum)
+	}
+	Message.Legend = legends
+	Message.Value = values
+	responseJSON, _ := json.Marshal(Message)
+	fmt.Printf("Message: %s\n", string(responseJSON))
+
+	ctx.Header("Access-Control-Expose-Headers", "Content-Range")
+	ctx.Header("Content-Range", "bytes 0-9/*")
+	ctx.JSON(http.StatusOK, Message)
+	conn.Close()
 }
