f39f4716f71763724bb108767586285de8fb037b
Alejandro Escanero Blanco <aescanero@gmail.com>
2023-05-21 13:13:54 +0200

    Add httpClient for authentication with token/JWT

--- apiserver/dashboard/src/dataProvider/rest.ts
+++ apiserver/dashboard/src/dataProvider/rest.ts
@@ -1,6 +1,26 @@
 import simpleRestProvider from 'ra-data-simple-rest';
+import { fetchUtils } from 'react-admin';
 
-const restProvider = simpleRestProvider('http://localhost:9090');
+const httpClient = (url: string, options: fetchUtils.Options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+    }
+    const token = localStorage.getItem('token');
+    console.log("token: " + token);
+    //const authData = token ? JSON.parse(token) : null;
+    //const authToken = authData ? authData.token : null;
+      if (token) {
+          const headers = new Headers(options.headers);
+          headers.set('Authorization', `Bearer ${token}`);
+          options.headers = headers;
+      }
+    
+    options.method = 'GET';
+
+    return fetchUtils.fetchJson(url, options);
+  };
+
+const restProvider = simpleRestProvider('http://localhost:9090', httpClient);
 
 const delayedDataProvider = new Proxy(restProvider, {
     get: (target, name, self) =>
